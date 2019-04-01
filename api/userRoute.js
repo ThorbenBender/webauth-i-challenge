206bf39a7e3f99d214b0a6713a41091d855b10d7
Thorben Bender <bender.thorben0@gmail.com>
2019-04-01 16:35:05 +0200

    login is working

--- api/userRoute.js
+++ api/userRoute.js
@@ -15,13 +15,29 @@ routes.post('/register', async (req, res) => {
 		const hash = bcrypt.hashSync(user.password, 10);
 
 		user.password = hash;
-		newUser = db('User').insert(user);
+		newUser = await db('User').insert(user);
 		if (user) {
 			res.status(201).json(user);
 		}
 	} catch (error) {
 		console.log(error);
 		res.status(500).json({ message: 'Error at register' });
+	}
+});
+
+routes.post('/login', async (req, res) => {
+	try {
+		const credentials = req.body;
+		const user = await db('User').where({ username: credentials.username });
+		console.log(user);
+		if (!credentials || !bcrypt.compareSync(credentials.password, user[0].password)) {
+			res.status(401).json({ message: 'You shall not pass!!!' });
+		} else {
+			res.status(200).send('Logged in');
+		}
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: 'Error at login' });
 	}
 });
 
