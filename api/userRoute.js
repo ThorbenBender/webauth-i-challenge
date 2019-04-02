b85854b7c97dd4b87f6ae6abd25ac13d8e030bbd
Thorben Bender <bender.thorben0@gmail.com>
2019-04-02 17:15:47 +0200

    mvp done

--- api/userRoute.js
+++ api/userRoute.js
@@ -2,6 +2,7 @@ const express = require('express');
 const knex = require('knex');
 const knexConfig = require('../knexfile').development;
 const bcrypt = require('bcryptjs');
+const isAuthed = require('./middleware/isAuth');
 
 const db = knex(knexConfig);
 
@@ -17,6 +18,7 @@ routes.post('/register', async (req, res) => {
 		user.password = hash;
 		newUser = await db('User').insert(user);
 		if (user) {
+			req.session.user = user;
 			res.status(201).json(user);
 		}
 	} catch (error) {
@@ -28,16 +30,40 @@ routes.post('/register', async (req, res) => {
 routes.post('/login', async (req, res) => {
 	try {
 		const credentials = req.body;
-		const user = await db('User').where({ username: credentials.username });
-		console.log(user);
-		if (!credentials || !bcrypt.compareSync(credentials.password, user[0].password)) {
+		const user = await db('User').where({ username: credentials.username }).first();
+		if (!credentials || !bcrypt.compareSync(credentials.password, user.password)) {
 			res.status(401).json({ message: 'You shall not pass!!!' });
 		} else {
+			req.session.user = user;
 			res.status(200).send('Logged in');
 		}
 	} catch (error) {
 		console.log(error);
 		res.status(500).json({ message: 'Error at login' });
+	}
+});
+
+routes.get('/users', isAuthed, async (req, res) => {
+	try {
+		users = await db('User');
+		res.status(200).json(users);
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: 'Error at getting the data' });
+	}
+});
+
+routes.get('/logout', async (req, res) => {
+	try {
+		if (req.session) {
+			err = await req.session.destroy();
+			if (err) {
+				res.status(200).json({ message: 'You are logged out' });
+			}
+		}
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: 'Error at logout' });
 	}
 });
 
